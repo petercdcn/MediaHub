939155cde5dcf28b6cc0babdf02e5bb6c71c7ccd
Shifan Wu <shifan.wu@concur.com>
2017-05-04 22:28:51 -0700

    Update project date and author.

--- MediaHub/MediaHub/MHDetailViewController.h
+++ MediaHub/MediaHub/MHDetailViewController.h
@@ -2,7 +2,7 @@
 //  MHDetailViewController.h
 //  MediaHub
 //
-//  Created by Kai Song on 4/29/17.
+//  Created by Shifan Wu on 5/1/17.
 //  Copyright © 2017 ShifanW. All rights reserved.
 //
 
--- MediaHub/MediaHub/MHListCell.h
+++ MediaHub/MediaHub/MHListCell.h
@@ -2,7 +2,7 @@
 //  TableViewCell.h
 //  MediaHub
 //
-//  Created by Wu, Shifan on 4/29/17.
+//  Created by Wu, Shifan on 5/1/17.
 //  Copyright © 2017 ShifanW. All rights reserved.
 //
 
